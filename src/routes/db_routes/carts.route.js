fdc81fff642ab36bb1075fb5abbb6b803e8303de
juanvolpee <138537881+juanvolpee@users.noreply.github.com>
2024-06-03 21:21:26 -0300

    Update carts.route.js

--- src/routes/db_routes/carts.route.js
+++ src/routes/db_routes/carts.route.js
@@ -4,6 +4,7 @@ const CartsService = require('../../services/db_services/carts.service');
 const cartsService = new CartsService();
 
 // GET /api/carts/:cid
+//Busca un carrito por su id y trae todos los productos que tiene con Populate
 router.get("/:cid", async (req, res) => {
     try {
         const cid = req.params.cid;
@@ -15,7 +16,7 @@ router.get("/:cid", async (req, res) => {
     }
 });
 
-// POST /api/carts
+// POST /api/carts este endpoint crea un carrito vacio
 router.post("/", async (req, res) => {
     try {
         const newCart = await cartsService.createCart();
@@ -26,7 +27,7 @@ router.post("/", async (req, res) => {
     }
 });
 
-// POST /api/carts/:cid/product/:pid
+// POST /api/carts/:cid/product/:pid Este endpoint agrega un producto a un carrito
 router.post("/:cid/product/:pid", async (req, res) => {
     const { cid, pid } = req.params;
     const { quantity } = req.body;
@@ -35,6 +36,56 @@ router.post("/:cid/product/:pid", async (req, res) => {
       const updatedCart = await cartsService.addProductsToCart(cid, pid, quantity);
       res.status(201).send({ status: "success", payload: updatedCart });
     } catch (error) {
+        res.status(500).send({ status: "error",  error: error.message });
+    }
+});
+
+//DELETE api/carts/:cid/products/:pid: Este endpoint eliminará un producto específico del carrito.
+router.delete('/:cid/products/:pid', async (req, res) => {
+    const { cid, pid } = req.params;
+    try {
+        await cartsService.removeProductFromCart(cid, pid);
+        res.status(204).send({ status: "success", payload: null });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "error",  error: error.message });
+    }
+});
+
+//PUT api/carts/:cid: Este endpoint actualizará el carrito con un arreglo de productos.
+router.put('/:cid', async (req, res) => {
+    const { cid } = req.params;
+    const products = req.body;
+    try {
+        const updatedCart = await cartsService.updateCart(cid, products);
+        res.status(200).send({ status: "success", payload: updatedCart });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "error",  error: error.message });
+    }
+});
+
+//PUT api/carts/:cid/products/:pid: Este endpoint actualizará la cantidad de un producto específico en el carrito.
+router.put('/:cid/products/:pid', async (req, res) => {
+    const { cid, pid } = req.params;
+    const { quantity } = req.body;
+    try {
+        const updatedCart = await cartsService.updateProductQuantity(cid, pid, quantity);
+        res.status(200).send({ status: "success", payload: updatedCart });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ status: "error",  error: error.message });
+    }
+});
+
+//DELETE api/carts/:cid: Este endpoint eliminará todos los productos del carrito.
+router.delete('/:cid', async (req, res) => {
+    const { cid } = req.params;
+    try {
+        await cartsService.clearCart(cid);
+        res.status(204).send({ status: "success", payload: null });
+    } catch (error) {
+        console.log(error);
         res.status(500).send({ status: "error",  error: error.message });
     }
 });
