2d78a2dee830ebd0b8ee80051b24b14779310a82
juanvolpee <138537881+juanvolpee@users.noreply.github.com>
2024-06-03 21:17:42 -0300

    Add files via upload

---
+++ src/config/express.config.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Configura el middleware de Express para parsear el cuerpo de las solicitudes HTTP
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Configura el middleware de Express para servir archivos estáticos
+app.use(express.static('public'));
+
+module.exports = app;
