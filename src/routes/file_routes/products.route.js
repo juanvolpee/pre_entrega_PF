b237837ea44c328990497f4d547db8193025688f
juanvolpee <138537881+juanvolpee@users.noreply.github.com>
2024-06-03 21:23:58 -0300

    Update products.route.js

--- src/routes/file_routes/products.route.js
+++ src/routes/file_routes/products.route.js
@@ -1,42 +1,70 @@
 const express = require('express');
 const router = express.Router();
-const CartManager = require('../../services/file_services/carts.manager');
-const cartManager = new CartManager();
+const ProductsManager = require('../../services/file_services/products.manager');
+const productsManager = new ProductsManager();
 
-// GET /api/carts/:cid
-router.get("/:cid", async (req, res) => {
-    try {
-        const cid = req.params.cid;
-        const cart = await cartManager.getCartById(cid);
-        res.send({ status: "success", payload: cart });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ status: "error",  error: error.message});
-    }
+// GET /api/products
+router.get('/', async (req, res) => {
+  const { limit, page, sort, query } = req.query;
+  try {
+    const products = await productsManager.getAllProducts(parseInt(limit, page, sort, query));
+    res.status(200).send({ status: "success", payload: products });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ status: "error",  error: error.message});
+  }
 });
 
-// POST /api/carts
-router.post("/", async (req, res) => {
-    try {
-        const newCart = await cartManager.createCart();
-        res.status(201).send({ status: "success", payload: newCart });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ status: "error",  error: error.message });
+// GET /api/products/:pid
+router.get('/:pid', async (req, res) => {
+  const { pid } = req.params;
+  try {
+    const product = await productsManager.getProductById(pid);
+    if (!product) {
+      return res.status(404).send({ status: "error",  error: 'Product not found' });
     }
+    res.status(200).send({ status: "success", payload: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ status: "error",  error: error.message});
+  }
 });
 
-// POST /api/carts/:cid/product/:pid
-router.post("/:cid/product/:pid", async (req, res) => {
-    const { cid, pid } = req.params;
-    const { quantity } = req.body;
+// POST /api/products
+router.post('/', async (req, res) => {
+  const newProduct = req.body;
+  try {
+    const product = await productsManager.addProduct(newProduct);
+    res.status(201).send({ status: "success", payload: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ status: "error",  error: error.message});
+  }
+});
 
-    try {
-      const updatedCart = await cartManager.addProductsToCart(cid, pid, quantity);
-      res.status(201).send({ status: "success", payload: updatedCart });
-    } catch (error) {
-        res.status(500).send({ status: "error",  error: error.message });
-    }
+// PUT /api/products/:pid
+router.put('/:pid', async (req, res) => {
+  const { pid } = req.params;
+  const updatedProductData = req.body;
+  try {
+    const updatedProduct = await productsManager.updateProductById(pid, updatedProductData);
+    res.status(201).send({ status: "success", payload: updatedProduct });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ status: "error",  error: error.message});
+  }
+});
+
+// DELETE /api/products/:pid
+router.delete('/:pid', async (req, res) => {
+  const { pid } = req.params;
+  try { 
+    await productsManager.deleteProductById(pid);
+     res.status(205).send({ status: "success", payload: null});
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ status: "error",  error: error.message});
+  }
 });
 
 module.exports = router;
