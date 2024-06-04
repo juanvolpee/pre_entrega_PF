0386be41cc6e536ae2d44b9aeff8f1eb05a7315e
juanvolpee <138537881+juanvolpee@users.noreply.github.com>
2024-06-03 21:58:41 -0300

    Update products.service.js

--- src/services/db_services/products.service.js
+++ src/services/db_services/products.service.js
@@ -1,29 +1,77 @@
-const productsModels = require('../../models/mongo_models/products.schema');
+const productsModels = require("../../models/mongo_models/products.schema");
 
 class ProductsService {
-    constructor() {
-        this.productsModel = productsModels;
+  constructor() {
+    this.productsModel = productsModels;
+  }
+
+  async getAllProducts(params) {
+    const {
+      limit = 4, // default limit = 4
+      page = 1, // default page = 1
+      sort = null,
+      query = null,
+      category = null,
+      status = null, //available
+    } = params;
+
+    const options = {
+      query: query,
+      page: Number(page),
+      limit: Number(limit),
+      sort: sort ? { price: sort === "asc" ? 1 : -1 } : {},
+      customLabels: {
+        docs: "products",
+        totalDocs: "totalProducts",
+      },
+    };
+
+    let searchQuery = {};
+
+    // $options: 'i' en MongoDB se utiliza para hacer que la búsqueda sea insensible a mayúsculas y minúsculas.
+    if (query) {
+      searchQuery.title = { $regex: query, $options: "i" };
     }
 
-    async getAllProducts(limit) {
-        return await this.productsModel.find().limit(limit);
+    if (category) {
+      searchQuery.category = { $regex: category, $options: "i" };
     }
 
-    async getProductById(productId) {
-        return await this.productsModel.findById(productId);
+    if (status !== null) {
+      searchQuery.status = status === "true";
     }
 
-    async addProduct(productData) {
-        return await this.productsModel.create(productData);
-    }
+    const result = await this.productsModel.paginate(searchQuery, options);
+    return result;
+  }
 
-    async updateProductById(productId, updatedProductData) {
-        return await this.productsModel.findByIdAndUpdate(productId, updatedProductData, { new: true });
-    }
+  async getProductById(pid) {
+    return await this.productsModel.findById(pid);
+  }
 
-    async deleteProductById(productId) {
-        return await this.productsModel.findByIdAndDelete(productId);
+
+  async addProduct(productData) {    
+    const productList = await this.getAllProducts({});
+    const productExist = productList.products.find(
+      (product) => product.code === productData.code
+    ); 
+    if (productExist) {
+      throw new Error( `El producto con el codigo: ${productData.code} ya existe` );     
     }
+    return await this.productsModel.create(productData);
+  }
+
+  async updateProductById(pid, updatedProductData) {
+    return await this.productsModel.findByIdAndUpdate(
+      pid,
+      updatedProductData,
+      { new: true }
+    );
+  }
+
+  async deleteProductById(pid) {
+    return await this.productsModel.findByIdAndDelete(pid);
+  }
 }
 
 module.exports = ProductsService;
